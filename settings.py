b7488f0ab49b05e2df05b49101567db914b04923
Kris Chambers <krischambers86@pm.me>
2023-12-31 11:36:07 -0500

    End game when out of ships

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ class Settings:
         self.bullets_allowed = 4
         
         # Alien settings
-        self.alien_speed = 0.5
-        self.fleet_drop_speed = 5
+        self.alien_speed = 1
+        self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
