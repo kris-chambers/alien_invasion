f136f6573a04e05acf0bd0b3590f3b6b7dde2992
Kris Chambers <krischambers86@pm.me>
2023-12-31 14:53:49 -0500

    added increase in difficulty between levels

--- alien_invasion.py
+++ alien_invasion.py
@@ -110,6 +110,7 @@ class AlienInvasion:
             # Destroy existing bullets and create new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
                 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -201,6 +202,9 @@ class AlienInvasion:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -219,6 +223,9 @@ class AlienInvasion:
     def _start_game(self):
         """Start a new game when the player presses P."""
         if not self.stats.game_active:
+            # Reset the game settings
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
